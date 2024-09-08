bf596c0d545cc0274f73d2f2f950602dd86ed9c8
faiq157 <faiqa5325@gmail.com>
2024-09-08 21:45:55 +0500

    signout add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+const base_URL = import.meta.env.VITE_APP_API_URL;
+
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(`${base_URL}/auth/logout`, null, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      localStorage.removeItem("token");
+      navigate("/login");
+    } catch (err) {
+      console.error("Error logging out:", err);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-7xl font-bold text-center">
-        This is E-Summery Website that Manage Your Notes in Education System
+    <div className="text-center">
+      <h1 className="text-7xl font-bold">
+        This is E-Summery Website that Manages Your Notes in Education System
       </h1>
+      <button
+        onClick={handleLogout}
+        className="mt-10 bg-red-500 text-white font-semibold py-2 px-4 rounded hover:bg-red-700"
+      >
+        Logout
+      </button>
     </div>
   );
 };
